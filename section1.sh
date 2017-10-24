30731c4b7f92c8b0e411ece2278d12da5f00902e
bayupermadi <4828046+bayupermadi@users.noreply.github.com>
2017-10-24 16:17:54 +0700

    Update section1.sh

--- section1.sh
+++ section1.sh
@@ -20,7 +20,7 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Ensure AIDE is installed (Scored)"
+echo "- 1.3.1 Ensure AIDE is installed (Scored)"
 check2=`rpm -q aide`
 
 if [ "$check2" == "package aide is not installed" ]
@@ -37,7 +37,7 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Implement Periodic Execution of File Integrity"
+echo "- 1.3.2 Ensure filesystem integrity is regularly checked (Scored)"
 check3=`crontab -u root -l | grep aide`
 
 if [ "$check3" == "0 5 * * * /usr/sbin/aide --check" ]
@@ -52,7 +52,7 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Enable SELinux in /boot/grub2/grub.cfg"
+echo "- 1.6.1.1 Ensure SELinux is not disabled in bootloader configuration (Scored)"
 check4a=`grep selinux=0 /boot/grub2/grub.cfg`
 check4b=`grep enforcing=0 /boot/grub2/grub.cfg`
 
@@ -69,7 +69,7 @@ echo""
 echo "==========================="
 echo ""
 
-echo "-  Set the SELinux State"
+echo "-  1.6.1.2 Ensure the SELinux state is enforcing (Scored)"
 check5=`grep SELINUX=enforcing /etc/selinux/config`
 
 if [ "$check5" == "SELINUX=enforcing" ]
@@ -85,14 +85,14 @@ echo""
 echo "==========================="
 echo ""
 
-echo "-  Remove SETroubleshoot"
+echo "-  1.6.1.4 Ensure SETroubleshoot is not installed (Scored) "
 check6=`rpm -q setroubleshoot`
 
 if [ "$check6" == "package setroubleshoot is not installed" ]
 then
         echo "-- okay"
 else
-        yum erase setroubleshoot -y
+        yum remove setroubleshoot -y
         echo "-- Its okay now"
 fi
 
@@ -100,7 +100,7 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Check for Unconfined Daemons"
+echo "- 1.6.1.6 Ensure no unconfined daemons exist (Scored)"
 check7=`ps -eZ | egrep "initrc" | egrep -vw "tr|ps|egrep|bash|awk" | tr ':' ' ' | awk '{print $NF }'`
 
 if [ "$check7" == "" ]
@@ -114,12 +114,13 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Set User/Group Owner on /boot/grub2/grub.cfg"
-check8=`stat -L -c "%u %g" /boot/grub2/grub.cfg | egrep "0 0"`
+echo "- 1.4.1 Ensure permissions on bootloader config are configured (Scored) "
+check8=`stat /boot/grub2/grub.cfg | grep root | grep root | grep 600`
 
 if [ "$check8" == "" ]
 then
 	chown root:root /boot/grub2/grub.cfg
+	chmod og-rwx /boot/grub2/grub.cfg
         echo "-- Its okay now"
 else
         echo "-- okay"
@@ -129,23 +130,8 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Set Permissions on /boot/grub2/grub.cfg"
-check9=`stat -L -c "%a" /boot/grub2/grub.cfg | egrep ".00"`
-
-if [ "$check9" == "" ]
-then
-        chmod og-rwx /boot/grub2/grub.cfg
-        echo "-- Its okay now"
-else
-        echo "-- okay"
-fi
-
-echo""
-echo "==========================="
-echo ""
-
-echo "- Restrict Core Dumps"
-check10a=`grep "hard core" /etc/security/limits.conf`
+echo "- 1.5.1 Ensure core dumps are restricted (Scored)"
+check10a=`grep "hard core" /etc/security/limits.conf /etc/security/limits.d/*`
 
 if [ "$check10a" == "" ]
 then
@@ -170,12 +156,13 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Enable Randomized Virtual Memory Region Placement"
+echo "- 1.5.3 Ensure address space layout randomization (ASLR) is enabled (Scored)"
 check11=`/sbin/sysctl kernel.randomize_va_space`
 
 if [ "$check11" == "" ]
 then
        	echo "kernel.randomize_va_space = 2" >> /etc/sysctl.conf
+	sysctl -w kernel.randomize_va_space=2
         echo "-- Its okay now"
 else
         echo "-- okay"
@@ -185,7 +172,7 @@ echo""
 echo "==========================="
 echo ""
 
-echo "-  Set the SELinux Policy"
+echo "-  1.6.1.3 Ensure SELinux policy is configured (Scored)"
 check12=`sudo grep SELINUXTYPE=targeted /etc/selinux/config`
 
 if [ $check12 == "SELINUXTYPE=targeted" ]
@@ -201,14 +188,14 @@ echo""
 echo "==========================="
 echo ""
 
-echo "-  Remove MCS Translation Service (mcstrans)"
+echo "-  1.6.1.5 Ensure the MCS Translation Service (mcstrans) is not installed (Scored)"
 check13=`rpm -q mcstrans`
 
 if [ "$check13" == "package mcstrans is not installed" ]
 then
         echo "-- okay"
 else
-	yum erase mcstrans
+	yum remove -y mcstrans
         echo "-- its okay now"
 fi
 
@@ -216,8 +203,8 @@ echo""
 echo "==========================="
 echo ""
 
-echo "-  Create Separate Partition for /tmp"
-check14=`grep "[[:space:]]/tmp[[:space:]]" /etc/fstab`
+echo "-  1.1.2 Ensure separate partition exists for /tmp (Scored) "
+check14=`mount | grep /tmp`
 
 if [ "$check14" == "" ]
 then
@@ -230,11 +217,11 @@ echo""
 echo "==========================="
 echo ""
 
-echo "-  Set noexec option for /tmp Partition"
-check14a=`grep "[[:space:]]/tmp[[:space:]]" /etc/fstab | grep noexec`
+echo "-  1.1.5 Ensure noexec option set on /tmp partition (Scored)"
+check14a=`mount | grep /tmp | grep noexec`
 check14b=`mount | grep "[[:space:]]/tmp[[:space:]]" | grep noexec`
 
-if [ "$check14a" == "" ] || [ "$check14b" == "" ] 
+if [ "$check14a" == "" ]
 then
         echo "-- its not okay, please do it manually"
 else
@@ -245,23 +232,8 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Bind Mount the /var/tmp directory to /tmp"
-check15a=`grep -e "^/tmp[[:space:]]" /etc/fstab | grep /var/tmp`
-check15b=`mount | grep -e "^/tmp[[:space:]]" | grep /var/tmp`
-
-if [ "$check15a" == "" ] || [ "$check15b" == "" ]
-then
-        echo "-- its not okay, please do it manually"
-else
-        echo "-- okay"
-fi
-
-echo""
-echo "==========================="
-echo ""
-
-echo "- Create Separate Partition for /var/log"
-check16=`grep "[[:space:]]/var/log/audit[[:space:]]" /etc/fstab`
+echo "- 1.1.11 Ensure separate partition exists for /var/log (Scored)"
+check16=`mount | grep /var/log`
 
 if [ "$check16" == "" ] 
 then
@@ -274,8 +246,8 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Create Separate Partition for /var/log/audit"
-check17=`grep "[[:space:]]/var/log/audit[[:space:]]" /etc/fstab`
+echo "- 1.1.12 Ensure separate partition exists for /var/log/audit (Scored)"
+check17=`mount | grep /var/log/audit`
 
 if [ "$check17" == "" ]
 then
@@ -288,8 +260,8 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Create Separate Partition for /home"
-check18=`grep "[[:space:]]/home[[:space:]]" /etc/fstab`
+echo "- 1.1.13 Ensure separate partition exists for /home (Scored) "
+check18=`mount | grep /home`
 
 if [ "$check18" == "" ]
 then
@@ -302,7 +274,7 @@ echo""
 echo "==========================="
 echo ""
 
-echo "- Add nodev Option to /home"
+echo "- 1.1.14 Ensure nodev option set on /home partition (Scored)"
 check19=`mount | grep /home | grep nodev`
 
 if [ "$check19" == "" ]
