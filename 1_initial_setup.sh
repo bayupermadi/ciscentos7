295c6ed4abcdb0a5f3842115bbfed7d0c0d5166a
bayupermadi <irwanhikmah@gmail.com>
2017-10-24 17:56:58 +0700

    export result

--- 1_initial_setup.sh
+++ 1_initial_setup.sh
@@ -8,6 +8,7 @@ then
 else
 	result1_1_2="OK"
 fi
+export result1_1_2
 
 # 1.1.5 Ensure noexec option set on /tmp partition (Scored)
 check1_1_5=`mount | grep /tmp | grep noexec`
@@ -17,16 +18,17 @@ then
 else
     result1_1_5="-- OK"
 fi
+export result1_1_5
 
 # 1.1.11 Ensure separate partition exists for /var/log (Scored)
 check1_1_11=`mount | grep /var/log`
-
 if [ "$check1_1_11" == "" ] 
 then
 	result1_1_11="ERR, Fix Manually"
 else
 	result1_1_11="OK"
 fi
+export result1_1_11
 
 # 1.1.12 Ensure separate partition exists for /var/log/audit (Scored)
 check1_1_12=`mount | grep /var/log/audit`
@@ -36,6 +38,7 @@ then
 else
 	result1_1_12="OK"
 fi
+export result1_1_12
 
 # 1.1.13 Ensure separate partition exists for /home (Scored)
 check1_1_13=`mount | grep /home`
@@ -45,6 +48,7 @@ then
 else
 	result1_1_13="OK"
 fi
+export result1_1_13
 
 # 1.1.14 Ensure nodev option set on /home partition (Scored)
 check1_1_14=`mount | grep /home | grep nodev`
@@ -54,6 +58,7 @@ then
 else
 	result1_1_14="OK"
 fi
+export result1_1_14
 
 # 1.2.3 Ensure gpgcheck is globally activated (Scored)
 check1_2_3=`grep gpgcheck /etc/yum.conf`
@@ -64,6 +69,7 @@ else
 	sed -i "s/gpgcheck=0/gpgcheck=1/g" /etc/yum.conf
 	result1_2_3="ERR, Fix Automatically"
 fi
+export result1_2_3
 
 # 1.3.1 Ensure AIDE is installed (Scored)
 check1_3_1=`rpm -q aide`
@@ -74,6 +80,7 @@ then
 else
 	result1_3_1="OK"
 fi
+export result1_3_1
 
 # 1.3.2 Ensure filesystem integrity is regularly checked (Scored)
 check1_3_2=`crontab -u root -l | grep aide`
@@ -84,6 +91,7 @@ else
     echo "0 5 * * * /usr/sbin/aide --check" | sudo tee -a /var/spool/cron/root
 	result1_3_2="ERR, Fix Automatically"
 fi
+export result1_3_2
 
 # 1.4.1 Ensure permissions on bootloader config are configured (Scored)
 check1_4_1=`stat /boot/grub2/grub.cfg | grep root | grep root | grep 600`
@@ -96,6 +104,7 @@ then
 else
     result1_4_1="OK"
 fi
+export result1_4_1
 
 # 1.5.1 Ensure core dumps are restricted (Scored)
 check1_5_1=`grep "hard core" /etc/security/limits.conf /etc/security/limits.d/*`
@@ -107,10 +116,10 @@ then
 else
     result1_5_1="OK"
 fi
+export result1_5_1
 
 # 1.5.3 Ensure address space layout randomization (ASLR) is enabled (Scored)
 check1_5_3=`/sbin/sysctl kernel.randomize_va_space`
-
 if [ "$check1_5_3" == "" ]
 then
     echo "kernel.randomize_va_space = 2" >> /etc/sysctl.conf
@@ -119,11 +128,11 @@ then
 else
     result1_5_3="OK"
 fi
+export result1_5_3
 
 # 1.6.1.1 Ensure SELinux is not disabled in bootloader configuration (Scored)
 check1_6_1_1a=`grep selinux=0 /boot/grub2/grub.cfg`
 check1_6_1_1b=`grep enforcing=0 /boot/grub2/grub.cfg`
-
 if [ "$check1_6_1_1a" == "" ] && [ "$check1_6_1_1b" == "" ]
 then
     result1_6_1_1="OK"
@@ -132,6 +141,7 @@ else
 	sed -i '/enforcing=0/d' /boot/grub2/grub.cfg
 	result1_6_1_1="ERR, Fix Automatically"
 fi 
+export result1_6_1_1
 
 # 1.6.1.2 Ensure the SELinux state is enforcing (Scored)
 check1_6_1_2=`grep SELINUX=enforcing /etc/selinux/config`
@@ -144,10 +154,10 @@ else
     sed -i 's/SELINUX=disabled/SELINUX=enforcing/g' /etc/selinux/config
 	result1_6_1_2="ERR, Fix Automatically"
 fi
+export result1_6_1_2
 
 # 1.6.1.4 Ensure SETroubleshoot is not installed (Scored)
 check1_6_1_4=`rpm -q setroubleshoot`
-
 if [ "$check1_6_1_4" == "package setroubleshoot is not installed" ]
 then
     result1_6_1_4="OK"
@@ -155,6 +165,7 @@ else
     yum remove setroubleshoot -y
 	result1_6_1_4="ERR, Fix Automatically"
 fi
+export result1_6_1_4
 
 # 1.6.1.5 Ensure the MCS Translation Service (mcstrans) is not installed (Scored)
 check1_6_1_5=`rpm -q mcstrans`
@@ -166,6 +177,7 @@ else
 	yum remove -y mcstrans
 	result1_6_1_5="ERR, Fix Automatically"
 fi
+export result1_6_1_5
 
 # 1.6.1.6 Ensure no unconfined daemons exist (Scored)
 check1_6_1_6=`ps -eZ | egrep "initrc" | egrep -vw "tr|ps|egrep|bash|awk" | tr ':' ' ' | awk '{print $NF }'`
@@ -176,5 +188,6 @@ then
 else
 	result1_6_1_6="ERR, Fix Manually"
 fi
+export result1_6_1_6
 
 bash 1_output.sh
