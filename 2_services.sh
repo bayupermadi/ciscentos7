ddc7b1be023435cd2cedc779714b510d742cd8ff
bayupermadi <none>
2017-10-27 12:34:07 +0700

    last 2.1.7 fix 1

--- 2_services.sh
+++ 2_services.sh
@@ -1,5 +1,5 @@
 # 2.1.1 Ensure chargen services are not enabled (Scored)
-check2_1_1=`systemctl list-unit-files | grep -e 'chargen-dgram\|chargen-stream' | enabled`
+check2_1_1=`systemctl list-unit-files | grep -e 'chargen-dgram\|chargen-stream' | grep enabled`
 if [ "$check2_1_1" != "" ]
 then
     result2_1_1="OK"
