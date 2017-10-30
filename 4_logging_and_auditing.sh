9cb95aa12a564cf8b8c8c898d42e7d40a1f20508
bayupermadi <none>
2017-10-30 17:10:52 +0700

    last 4.1.1.3 fix 2

--- 4_logging_and_auditing.sh
+++ 4_logging_and_auditing.sh
@@ -12,10 +12,10 @@ fi
 export result4_1_1_1
 
 # 4.1.1.2 Ensure system is disabled when audit logs are full (Scored)
-check4_1_1_2a=`grep space_left_action /etc/audit/auditd.conf`
-check4_1_1_2b='grep action_mail_acct /etc/audit/auditd.conf'
-check4_1_1_2c='grep admin_space_left_action /etc/audit/auditd.conf'
-if [ "$check4_1_1_2a" == "space_left_action = email"  ] && [ "$check4_1_1_2b" == "action_mail_acct = root"  ] && [ "$check4_1_1_2c" == "admin_space_left_action = halt"  ]
+check4_1_1_2a=`cat /etc/audit/auditd.conf | grep "space_left_action = email"`
+check4_1_1_2b='cat /etc/audit/auditd.conf | grep "action_mail_acct = root"'
+check4_1_1_2c='cat /etc/audit/auditd.conf | grep "admin_space_left_action = halt "'
+if [ "$check4_1_1_2a" != ""  ] && [ "$check4_1_1_2b" != ""  ] && [ "$check4_1_1_2c" != ""  ]
 then
     result4_1_1_2="OK"
 else
